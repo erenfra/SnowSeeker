9beebb8b4e0fe9336f74354e3d66dff5d7e1df52
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-23 22:43:24 -0600

    day97 commit

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: Text(resort.name)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.black, lineWidth: 1))
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationBarTitle("Resorts")
+            WelcomeView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
