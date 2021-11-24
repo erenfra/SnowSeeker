6f4891209c7651e1c7b4b3420c712e2f68610ce1
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-11-23 20:31:48 -0600

    first commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Renato Oliveira Fraga on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
